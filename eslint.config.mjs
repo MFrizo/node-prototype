9c8d1ea4a51298be1175c606227b7c8109b19958
MFrizo <matheus.fonseca@quintoandar.com.br>
2026-01-21 23:26:19 -0300

    Adjust eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,45 @@ export default [
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
+  {
+    ignores: ['dist/**', '**/*.js'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      globals: {
+        console: 'readonly',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-extraneous-class': [
+        'error',
+        {
+          allowStaticOnly: true,
+        },
+      ],
+      '@typescript-eslint/no-empty-object-type': [
+        'error',
+        {
+          allowInterfaces: 'always',
+        },
+      ],
+    },
+  },
+  {
+    files: ['src/__tests__/**/*.ts'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        jest: 'readonly',
+        console: 'readonly',
+      },
+    },
+  },
 ];
